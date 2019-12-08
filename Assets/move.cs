33905d101229c80231cf61cd33a50b1f45259ed9
Bethany Drake <bethanyd@thoughtworks.com>
2019-12-08 19:24:41 +1100

    move script and simple level

---
+++ Assets/move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public float turnSpeed = 1;
+    public float speed = 2;
+
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float turnAmount = Input.GetAxis("Horizontal")* turnSpeed;
+        Debug.Log(turnAmount);
+        rb.velocity = gameObject.transform.forward * speed;
+        gameObject.transform.Rotate(new Vector3(0, turnAmount, 0));
+    }
+
+}
